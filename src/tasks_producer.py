eb44ccffbe5c3e42603c95977fb9203bf2306b1f
Tosin <keeks@Keekss-Air.fritz.box>
2021-12-29 14:17:11 +0100

    Handle send_webhooks exceptions

--- src/tasks_producer.py
+++ src/tasks_producer.py
@@ -8,7 +8,7 @@ import uuid
 from faker import Faker
 from faker.providers import BaseProvider
 
-import src.config.config
+from src.config import config
 
 
 # Definde TaskProvider
@@ -39,3 +39,33 @@ def produce_task(batch_id, task_id):
         'priority': fake_tasks.task_priority()
     }
     return message
+
+
+def send_webhook(msg):
+    """Sends a webhook to the specified URL
+
+    Args:
+        msg ([type]): Task details
+    """
+
+    try:
+        # Post webhook Message
+        response = requests.post(
+            config.WEBHOOK_RECEIVER_URL, 
+            data=json.dumps(
+                obj=msg,
+                sort_keys=True,
+                default=str)
+            ,headers={'Content-Type':'application/json'},
+            timeout=1.0)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print(f"An HTTP error occured: {error.message}")
+    except requests.exceptions.ConnectionError as error:
+         print(f"A Connection error occured: {error.message}")
+    except requests.exceptions.Timeout as error:
+        print(f"A Timeout error occured: {error.message}")
+    except requests.exceptions.RequestException as error:
+         print(f"An Unknown error occured: {error.message}")
+    else:
+        return response.status_code
